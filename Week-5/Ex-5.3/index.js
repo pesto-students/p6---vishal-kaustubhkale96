4848340743d602f485999b1a544edabd8aa7b3ea
Kaustubh Kale <kaustubhkale96@gmail.com>
2022-11-13 08:55:27 +0530

    Added another method to check duplicate numbers

--- Week-5/Ex-5.3/index.js
+++ Week-5/Ex-5.3/index.js
@@ -7,3 +7,8 @@ function hasDuplicate(arr) {
 
 console.log(hasDuplicate([1, 2, 1, 3, 4, 3, 5, 6, 5]));
 console.log(hasDuplicate([1, 2, 3, 4, 5, 6, 7]));
+
+function hasDuplicate1(nums) {
+    return nums.some((e, i) => nums.indexOf(e) != i)
+};
+console.log(hasDuplicate1([1, 2, 1, 3, 4, 3, 5, 6, 5]))
